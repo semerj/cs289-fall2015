c0d77df18bf1931bebc6b22d91a8434d7d2a2e02
semerdj <jsemer@gmail.com>
2015-12-04 20:57:47 -0800

    move matrix mult outside of inner loop

--- hw7/ALSLatentFactorModel.py
+++ hw7/ALSLatentFactorModel.py
@@ -34,25 +34,25 @@ class ALSLatentFactorModel:
         for _ in range(self.num_iters):
             # user factors
             VtV = self.V.T.dot(self.V)
+            λI = np.diag(self.λ*np.ones(self.d))
+            VtV_λI = np.linalg.inv(VtV + λI)
             for u in range(num_users):
-                # get indices of non-zero scores
                 user_ratings = X[u,:]
                 not_nan_ind = np.where(~np.isnan(user_ratings))[0]
                 r_ui = user_ratings[not_nan_ind]
                 V_i = self.V[not_nan_ind]
-                # solve analytically
-                λI = np.diag(self.λ*np.ones(self.d))
-                self.U[u] = (np.linalg.inv(VtV + λI)).dot(r_ui.dot(V_i))
+                self.U[u] = VtV_λI.dot(r_ui.dot(V_i))
 
             # item factors
             UtU = self.U.T.dot(self.U)
+            λI = np.diag(self.λ*np.ones(self.d))
+            UtU_λI = np.linalg.inv(UtU + λI)
             for i in range(num_items):
                 item_ratings = X[:,i]
                 not_nan_ind = np.where(~np.isnan(item_ratings))[0]
                 r_ui = item_ratings[not_nan_ind]
                 U_i = self.U[not_nan_ind]
-                λI = np.diag(self.λ*np.ones(self.d))
-                self.V[i] = (np.linalg.inv(UtU + λI)).dot(r_ui.dot(U_i))
+                self.V[i] = UtU_λI.dot(r_ui.dot(U_i))
 
     def predict(self):
         R_hat = self.U.dot(self.V.T)
